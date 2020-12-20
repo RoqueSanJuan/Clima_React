c2cae1b66e7e84994df4885cf2fbb18bbadc2826
roque <roque.m.sanjuan@gmail.com>
2020-12-19 22:26:04 -0300

    Cambio Api a https

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
       if(consultar) {
 
         const appId = '1f90c57d0357840a1381e0eaaaf90a3a';
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
         guardarResultado(resultado);
